21929dff1c794e3b39a65552b08b1f08fa729258
aditya26raj01 <aditya26raj01@gmail.com>
2024-07-11 19:51:01 +0530

    initial

---
+++ functions/express-server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const connectDB = require("../config/connectDB");
+const router = require("../routes/index");
+const cookiesParser = require("cookie-parser");
+
+const app = express();
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  })
+);
+app.use(express.json());
+app.use(cookiesParser());
+
+const PORT = process.env.PORT || 8080;
+
+app.get("/", (request, response) => {
+  response.json({
+    message: "Server running at " + PORT,
+  });
+});
+
+// API endpoints
+app.use("/api", router);
+
+module.exports.handler = async (event, context) => {
+  await connectDB();
+  const serverlessExpress = require("@vendia/serverless-express");
+  const server = serverlessExpress.createServer(app);
+  return serverlessExpress.proxy(server, event, context);
+};
